49d42ae2654ea0615403efe28801b603b841b176
mpia <mpiazaniboni@gmail.com>
2017-09-14 14:35:42 -0300

    Librerias y cositas del carrito

--- footer.php
+++ footer.php
@@ -5,6 +5,8 @@
     <script src="js/principal.js"></script>
     <script src="https://use.fontawesome.com/e71eeac67c.js"></script>
     <script type="text/javascript" src="js/wow.js"></script> 
+    <!--Cookies-->
+    <script type="text/javascript" src="js/js.cookie.js"></script>
     <script>
               new WOW().init();
               </script>

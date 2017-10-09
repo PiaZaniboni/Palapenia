ca0a8c3fd5fd229fb1aaf74d5222b19853ac9bd9
joaquina <joaquisalgadoc0795@gmail.com>
2017-10-09 15:33:35 -0300

    boton

--- contacto.php
+++ contacto.php
@@ -24,8 +24,8 @@
                                 <div class="form-group">
                                    <textarea name="message" class="form-control" id="description" placeholder="..." required></textarea>
                                 </div>
-                                <div class="btn-submit">
-                                    <a class="submit-mail" href="">Enviar</a>
+                                <div class="">
+                                    <a class="btn btn-default submit " href="">Enviar</a>
                                     <p class="frm-message"></p>
                                 </div>
                             </fieldset>
